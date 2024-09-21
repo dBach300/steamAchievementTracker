ab29e0f4b06513c66951d94c5cd220ec680933f2
Dylan Bachman <bachman_dylan@yahoo.com>
2024-09-20 23:38:58 -0400

    initial code for get request from steam api

---
+++ steamAchievementTracker/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace steamAchievementTracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGetAchievements_Click(object sender, EventArgs e)
+        {
+            string apiKey = "api dev key here"; // api dev key
+            string steamId = "steam id here"; // User's Steam ID
+            string appId = "app id here"; // Your game’s app ID
+
+            SteamApi steamApi = new SteamApi(apiKey, steamId);
+
+            try
+            {
+                string achievementsJson = await steamApi.GetAchievementsAsync(appId);
+                // Deserialize
+                AchievementsResponse achievements = JsonConvert.DeserializeObject<AchievementsResponse>(achievementsJson);
+                MessageBox.Show(achievementsJson);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ steamAchievementTracker/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace steamAchievementTracker
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+    // achievement class
+    public class Achievement
+    {
+        public string Name { get; set; }
+        public bool Achieved { get; set; }
+    }
+    // gets/sets for achievements in api response
+    public class AchievementsResponse
+    {
+        public List<Achievement> Achievements { get; set; }
+    }
+    // class for steamAPI
+    public class SteamApi
+    {
+        private readonly string apiKey;
+        private readonly string steamId;
+
+        public SteamApi(string apiKey, string steamId)
+        {
+            this.apiKey = apiKey;
+            this.steamId = steamId;
+        }
+        // takes variable values for appID, steamID, and apiKey and calls API address - waits for reponse of user/achievement info
+        public async Task<string> GetAchievementsAsync(string appId)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = $"http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid={appId}&steamid={steamId}&key={apiKey}";
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+
+}
